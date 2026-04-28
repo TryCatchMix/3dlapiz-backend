7c36e460b3cb3623ee89c7d56486657a11719120
42Alberto <albertomunozgarcia.dev@gmail.com>
2026-04-29 00:14:08 +0200

    feat: add middleware alias for EnsureUserIsAdmin in application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+   ->withMiddleware(function (Middleware $middleware) {
+    $middleware->alias([
+        'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
